6f0cd1d1a5190a22000f1278349cf54250d18dcf
TiffyAI <quentenmeyer@gmail.com>
2025-04-29 20:52:00 +0200

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,20 @@
+﻿function showPopup(message, duration = 3000) {
+  const popup = document.getElementById("popup");
+  popup.innerText = message;
+  popup.classList.remove("hidden");
+
+  setTimeout(() => {
+    popup.classList.add("hidden");
+  }, duration);
+}
+
+function showLoading(message = "Loading...") {
+  const loading = document.getElementById("loading");
+  loading.innerText = message;
+  loading.classList.remove("hidden");
+}
+
+function hideLoading() {
+  const loading = document.getElementById("loading");
+  loading.classList.add("hidden");
+}
