0d871661795dea663e757ab1506a4747d9f7339c
TiffyAI <quentenmeyer@gmail.com>
2025-04-29 20:53:29 +0200

    Add files via upload

---
+++ disconnect.js
@@ -0,0 +1,8 @@
+﻿if (window.ethereum) {
+  window.ethereum.on("disconnect", () => {
+    showPopup("Wallet disconnected!");
+    setTimeout(() => {
+      location.reload();
+    }, 1500);
+  });
+}
